e4c273341369e9eda447ed13c7c8de9a598d8dd3
kranthi418 <kranthinaik09@gmail.com>
2026-05-11 23:50:07 +0530

    fix CORS for production

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV !== 'test') app.use(morgan('dev'));
